ead69f074795a7c5c146c4b99940a6f6bbd11fc5
Yu Lu <595129882@qq.com>
2016-10-28 00:32:12 -0700

    修改测试时发现的问题

--- recive.c
+++ recive.c
@@ -71,7 +71,7 @@ struct _rule_table{
 int main()
 {
 
-    int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int server_sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     struct sockaddr_in server_sockaddr;
     server_sockaddr.sin_family = AF_INET;
@@ -98,78 +98,94 @@ int main()
     char buffer[BUFFER_SIZE];
     struct sockaddr_in client_addr;
     socklen_t length = sizeof(client_addr);
-
-    int conn = accept(server_sockfd, (struct sockaddr *)&client_addr, &length);
-    if (conn < 0){
-        perror("connect");
-        exit(1);
-    }
-    printf("%s\n", inet_ntoa(client_addr.sin_addr));
-    struct _rule_table recvRule;
-    int i;
-    int j;
-    while (1){
-        memset(buffer, 0, sizeof(buffer));
-        int len = recv(conn, buffer, sizeof(buffer), 0);
-        if (len > 0)
-            printf("recv len:%d \n", len);
-        memcpy(&recvRule, buffer, len);
-        //memcpy(&recvRule, buffer, sizeof(struct _rule_table));
-        if (len > 0){
-            //printf("length:%d  priority:%d  proto:%d\n", recvRule.length, recvRule.priority, recvRule.basic.proto);
-            //printf("type: %d, desc: %d  ", recvRule.acttype, recvRule.actdesc);
-            printf("properties allow:%d    properties length: %d\n", recvRule.properties.allow, recvRule.properties.length);
-            for (i=0; i<recvRule.properties.length; i++){
-                //printf("op: %d, name: %s, length: %d\n", recvRule.properties.aclVar[i].op, \
-                       recvRule.properties.aclVar[i].name, recvRule.properties.aclVar[i].length );
-                printf("type: %d \n", recvRule.properties.aclVar[i].type);
-                switch(recvRule.properties.aclVar[i].type){
-                    case 1:
-                    case 2:
-                    case 3: // ip
-                        printf("ruleip, src:%s   ", inet_ntoa(recvRule.basic.src_ip));
-                        printf("dst:%s   ", inet_ntoa(recvRule.basic.dst_ip));
-                        printf("mask:%s\n", inet_ntoa(recvRule.basic.mask));
-                        for(j=0; j<recvRule.properties.aclVar[i].length; j++){
-                            printf("1: %s  ", inet_ntoa(recvRule.properties.aclVar[i].data[j].ip_data.addr1));
-                            printf("2: %s  ", inet_ntoa(recvRule.properties.aclVar[i].data[j].ip_data.addr2));
-                            printf("mask: %s\n", inet_ntoa(recvRule.properties.aclVar[i].data[j].ip_data.mask));
-                        }
+    while(1){
+        int conn = accept(server_sockfd, (struct sockaddr *)&client_addr, &length);
+        if (conn < 0){
+            perror("connect");
+            exit(1);
+        }
+        printf("%s\n", inet_ntoa(client_addr.sin_addr));
+        struct _rule_table recvRule;
+        int i;
+        int j;
+        int recv_num = 0;
+        int len = 0;
+        char sendOK[10];
+        while (1){
+            memset(buffer, 0, sizeof(buffer));
+            len = recv(conn, buffer, sizeof(buffer), 0);
+            if (len > 0){
+                if(len == 4){
+                    buffer[len] = '\0';
+                    printf("overbuf: %s\n", buffer);
+                    if (strcmp(buffer, "over") == 0){;
+                        recv_num = 0;
+                        close(conn);
+                        printf("change recv num%d\n", recv_num);
                         break;
-                    case 4:
-                    case 5: // domain
-                        for(j=0; j<recvRule.properties.aclVar[i].length; j++){
-                            printf("domain: %s\n", recvRule.properties.aclVar[i].data[j].domain);
-                        }
-                        break;
-                    case 8: //time
-                        for(j=0; j<recvRule.properties.aclVar[i].length; j++){
-                            printf("weekbits: %d  ", recvRule.properties.aclVar[i].data[j].time.weekbits);
-                            printf("start: %d  ", recvRule.properties.aclVar[i].data[j].time.start);
-                            printf("stop: %d\n", recvRule.properties.aclVar[i].data[j].time.stop);
-                        }
-                        break;
-                    case 11: //port
-                        printf("ruleport, port1:%d   port2:%d\n", recvRule.basic.dpts[0], recvRule.basic.dpts[1]);
-                        for(j=0; j<recvRule.properties.aclVar[i].length; j++)
-                            printf("i: %d, j: %d\n", recvRule.properties.aclVar[i].data[j].intrange.i, recvRule.properties.aclVar[i].data[j].intrange.j);
-                        break;
-                    case 17: //proto
-                        for(j=0; j<recvRule.properties.aclVar[i].length; j++)
-                            printf("i: %d\n", recvRule.properties.aclVar[i].data[j].proto);
-                        break;
-                    case 18: //method
-                        for(j=0; j<recvRule.properties.aclVar[i].length; j++)
-                            printf("i: %d\n", recvRule.properties.aclVar[i].data[j].method);
-                        break;
-                    default:
-                        break;
+                    }
+                }
+                recv_num ++;
+                printf("recv len:%d recv mun:%d\n", len, recv_num);
+                memset(&recvRule,0,sizeof(struct _rule_table));
+                memcpy(&recvRule, buffer, len);
+                strcpy(sendOK,"ok");
+                send(conn, sendOK, strlen(sendOK), 0);
+                //printf("length:%d  priority:%d  proto:%d\n", recvRule.length, recvRule.priority, recvRule.basic.proto);
+                //printf("type: %d, desc: %d  ", recvRule.acttype, recvRule.actdesc);
+                printf("properties allow:%d    properties length: %d\n", recvRule.properties.allow, recvRule.properties.length);
+                for (i=0; i<recvRule.properties.length; i++){
+                    //printf("op: %d, name: %s, length: %d\n", recvRule.properties.aclVar[i].op, \
+                           recvRule.properties.aclVar[i].name, recvRule.properties.aclVar[i].length );
+                    printf("type: %d \n", recvRule.properties.aclVar[i].type);
+                    switch(recvRule.properties.aclVar[i].type){
+                        case 1:
+                        case 2:
+                        case 3: // ip
+                            printf("ruleip, src:%s   ", inet_ntoa(recvRule.basic.src_ip));
+                            printf("dst:%s   ", inet_ntoa(recvRule.basic.dst_ip));
+                            printf("mask:%s\n", inet_ntoa(recvRule.basic.mask));
+                            for(j=0; j<recvRule.properties.aclVar[i].length; j++){
+                                printf("1: %s  ", inet_ntoa(recvRule.properties.aclVar[i].data[j].ip_data.addr1));
+                                printf("2: %s  ", inet_ntoa(recvRule.properties.aclVar[i].data[j].ip_data.addr2));
+                                printf("mask: %s\n", inet_ntoa(recvRule.properties.aclVar[i].data[j].ip_data.mask));
+                            }
+                            break;
+                        case 4:
+                        case 5: // domain
+                            for(j=0; j<recvRule.properties.aclVar[i].length; j++){
+                                printf("domain: %s\n", recvRule.properties.aclVar[i].data[j].domain);
+                            }
+                            break;
+                        case 8: //time
+                            for(j=0; j<recvRule.properties.aclVar[i].length; j++){
+                                printf("weekbits: %d  ", recvRule.properties.aclVar[i].data[j].time.weekbits);
+                                printf("start: %d  ", recvRule.properties.aclVar[i].data[j].time.start);
+                                printf("stop: %d\n", recvRule.properties.aclVar[i].data[j].time.stop);
+                            }
+                            break;
+                        case 11: //port
+                            printf("ruleport, port1:%d   port2:%d\n", recvRule.basic.dpts[0], recvRule.basic.dpts[1]);
+                            for(j=0; j<recvRule.properties.aclVar[i].length; j++)
+                                printf("i: %d, j: %d\n", recvRule.properties.aclVar[i].data[j].intrange.i, recvRule.properties.aclVar[i].data[j].intrange.j);
+                            break;
+                        case 17: //proto
+                            for(j=0; j<recvRule.properties.aclVar[i].length; j++)
+                                printf("i: %d\n", recvRule.properties.aclVar[i].data[j].proto);
+                            break;
+                        case 18: //method
+                            for(j=0; j<recvRule.properties.aclVar[i].length; j++)
+                                printf("i: %d\n", recvRule.properties.aclVar[i].data[j].method);
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
         }
     }
-    close(conn);
     close(server_sockfd);
 
     return 0;
 }
+
